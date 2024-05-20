421adb506f5fd92a07fa0a8c17e070dec1fd8cda
unknown <gangagowriselva@gmail.com>
2024-05-20 19:10:41 +1000

    test 111

--- May20_24TurnupPortal/Program.cs
+++ May20_24TurnupPortal/Program.cs
@@ -1,6 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-//Test1234
+//test111
 
 IWebDriver driver = new ChromeDriver();
 driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2fTimeMaterial");
@@ -10,3 +10,9 @@ IWebElement passwordElement = driver.FindElement(By.Id("Password"));
 passwordElement.SendKeys("123123");
 IWebElement LoginButtonElement = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
 LoginButtonElement.Click();
+//IWebElement StaffButtonElement = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[4]/a"));
+//StaffButtonElement.Click();
+IWebElement CreateNewButtonElement = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+CreateNewButtonElement.Click();
+IWebElement StaffButtonElement = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[4]/a"));
+StaffButtonElement.Click();
