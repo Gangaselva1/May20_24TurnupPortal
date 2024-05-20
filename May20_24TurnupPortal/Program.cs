d58e04555bd951452a84430830d7f2d59bb4d0c1
unknown <gangagowriselva@gmail.com>
2024-05-20 17:06:04 +1000

    Test

---
+++ May20_24TurnupPortal/Program.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2fTimeMaterial");
+IWebElement userNameElement = driver.FindElement(By.Id("UserName"));
+userNameElement.SendKeys("hari");
+IWebElement passwordElement = driver.FindElement(By.Id("Password"));
+passwordElement.SendKeys("123123");
+IWebElement LoginButtonElement = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+LoginButtonElement.Click();
